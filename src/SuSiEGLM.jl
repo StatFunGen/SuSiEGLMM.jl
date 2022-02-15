859d599b3d245ed9931f3c89c69adfc0cf83bbf7
Hyeonju Kim <hyeonjukm01@gmail.com>
2022-02-15 16:50:37 -0500

    add susie-glm_julia vignette and update dependency

--- src/SuSiEGLM.jl
+++ src/SuSiEGLM.jl
@@ -29,7 +29,44 @@ return result
 
 end 
 
+"""
 
+fineQTL_glm(G::GenoInfo,y::Vector{Float64},X::Matrix{Float64},X₀::Union{Matrix{Float64},Vector{Float64}}=ones(length(y),1)
+;L::Int64=10,Π::Vector{Float64}=[1/size(X,2)],tol=1e-4)
+
+
+
+Performs SuSiE (Sum of Single Effects model) GLM fine-mapping analysis for a binary trait (logistic regression).
+
+
+
+# Arguments
+
+- `G` : a Type of struct, `GenoInfo`. See [`GenoInfo`](@ref).
+- `y` : a n x 1 vector of  binary trait
+- `X` : a n x p matrix of genetic markers selected from QTL analysis
+- `X₀`: a n x 1 vector or n x c matrix of covariates.  The intercept is default if no covariate is added.
+
+## Keyword Arguments
+
+- `L` : an integer. The number of single effects for SuSiE implementation. Default is `10`.
+- `Π` : a p x 1 vector of prior inclusion probabilities for SuSiE.  Default is `1/p`, where `p = size(X,2)`. If different probabilities are added to SNPs, the length of Π should be `p`.
+- `tol`: tolerance. Default is `1e-4`. 
+
+# Output
+
+ Returns a Type of struct, `ResGLM` per Chromosome (`p1 = # of SNP in a Chromosome`), which includes 
+
+-  `ξ` : a n x 1 vector of variational parameters to fit a logitstic function
+-  `β` : a c x 1 vector of fixed effects for covariates
+-  `σ0` : a L x 1 vector of hyper-parameters for prior variances for SuSiE
+-  `elbo`: a p1 x 1 vector of evidence lower bound (ELBO)
+-  `α` : a p1 x L matrix of posterior inclusion probabilities of SuSiE
+-  `ν` : a p1 x L matrix of posterior mean of SuSiE
+-  `Σ` : a p1 x L matrix of posterior variances of SuSiE
+
+
+"""
 function fineQTL_glm(G::GenoInfo,y::Vector{Float64},X::Matrix{Float64},
     X₀::Union{Matrix{Float64},Vector{Float64}}=ones(length(y),1);L::Int64=10,Π::Vector{Float64}=[1/size(X,2)],tol=1e-4)
 
