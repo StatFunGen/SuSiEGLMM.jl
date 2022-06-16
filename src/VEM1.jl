5d4a14b71f93fa31f48c137749d927823bf6697e
Hyeonju Kim <hyeonjukm01@gmail.com>
2022-06-16 01:41:06 -0400

    corrected 'H1: including b' functions and added two versions of testStat functions for score test

--- src/VEM1.jl
+++ src/VEM1.jl
@@ -13,7 +13,7 @@
 
 ## include only integration out of β for glmms (H0 $ H1:QTL)
 
-export intOut,intβOut,emNull,emAlt,glmmNull,glmm1Snp,scan1SNP, guessξ0, Approx
+export intOut,intβOut,emNull,emAlt,glmmNull,glmm1Snp, guessξ0, Approx, scan1SNP
 
 
 function intB!(beta::Vector{Float64},M::Matrix{Float64},
@@ -90,11 +90,11 @@ function postB!(b1::Vector{Float64},σ1::Vector{Float64},Badj::intOut,ghat::Vect
         
      Λ=convert(Matrix{Float64},Diagonal(Badj.λ));
     σ1[:].=1.0/(Xt'BLAS.symv('U',Λ,Xt)+1.0/σ0[1])
-    b1[:].=Xt'(Badj.Ŷ-BLAS.symv('U',Λ,ghat))
+    b1[:].=Xt'*(Badj.Ŷ-BLAS.symv('U',Λ,ghat))
     lmul!(σ1[1],b1)
 end
 
-function emB!(b2::Vector{Float64},σ0_new::Vector{Float64},b1::Vector{Float64},σ1::Vector{{Float64}})
+function emB!(b2::Vector{Float64},σ0_new::Vector{Float64},b1::Vector{Float64},σ1::Vector{Float64})
 # one of them will be deleted later since they are the same each other.
     b2[:] = b1.^2+ σ1
     σ0_new[:].= mean(b2)
@@ -126,7 +126,7 @@ function mStep!(ξ_new::Vector{Float64},b1::Vector{Float64},σ1::Vector{Float64}
      mu[:] = mu + ghat
      
      Sig = convert(Matrix{Float64}, Diagonal(Vg))
-     Sig = Symmetric(BLAS.syr!('U',σ1[1],Xt,Sig))
+     Sig = convert(Matrix{Float64},Symmetric(BLAS.syr!('U',σ1[1],Xt,Sig)))
                 
     mul!(M1,BLAS.symm('R','U',Sig,Badj.M),Badj.M') #M*Sig*M'      
 
@@ -150,22 +150,27 @@ function ELBO(ξ_new::Vector{Float64},τ2_new::Vector{Float64},Badj::intOut,ghat
     # println("τ2_new: $(τ2_new)")
     
     gl = -0.5*(n*log(τ2_new[1])+ sum(log.(S)-log.(Vg))- 1.0 + sum(ghat2./S)/τ2_new[1]) # g
-    f=open(homedir()*"/GIT/susie-glmm/SuSiEGLMM.jl/test/est_elbo0.txt","a")
-    writedlm(f,[τ2_new[1] ll lbeta gl ll+gl+lbeta])
-    close(f)
+    # f=open(homedir()*"/GIT/susie-glmm/SuSiEGLMM.jl/test/est_elbo0.txt","a")
+    # writedlm(f,[τ2_new[1] ll lbeta gl ll+gl+lbeta])
+    # close(f)
 
     return ll+gl+lbeta
     
 end
 
 # for QTL
-function ELBO(ξ_new::Vector{Float64},τ2_new::Vector{Float64},σ0_new::Vector{Float64},Badj::intOut,b2::Vector{Float64},σ1::Vector{Float64},ghat::Vector{Float64},
+function ELBO(ξ_new::Vector{Float64},τ2_new::Vector{Float64},σ0_new::Vector{Float64},Badj::intOut,
+    b1::Vector{Float64},b2::Vector{Float64},σ1::Vector{Float64},ghat::Vector{Float64},
     ghat2::Vector{Float64},Vg::Vector{Float64},S::Vector{Float64},Xt::Vector{Float64},
     Xy₀::Vector{Float64},Σ₀::Matrix{Float64},n::Int64)
      
     
-      ll0 = ELBO(ξ_new,τ2_new,Badj,ghat,ghat2,Vg,S,Xy₀,Σ₀,n)
-      @fastmath @inbounds @views lb = Badj.Ŷ'(Xt*b1[1]) - 0.5(log(σ0_new[1])-log(σ1[1])-1.0+b2[1]/σ0_new[1])
+      ll0 = ELBO(ξ_new,τ2_new,Badj,ghat,ghat2,Vg,S,Xy₀,Σ₀,n) #loglik for H0
+    #   @fastmath @inbounds @views 
+      lb = Badj.Ŷ'*(Xt*b1[1]) - 0.5(log(σ0_new[1])-log(σ1[1])-1.0+b2[1]/σ0_new[1]) #loglik for b
+      f=open(homedir()*"/GIT/susie-glmm/SuSiEGLMM.jl/test/est_elbo1.txt","a")
+    writedlm(f,[τ2_new[1] b1[1] ll0 lb ll0+lb])
+    close(f)
 
       return ll0+lb
    
@@ -196,7 +201,7 @@ function emNull(yt,Xt₀,S,τ2,ξ,Σ₀;tol::Float64=1e-4)
     # Vβ̂inv,Badj = covarAdj(Xy₀,yt,Xt₀,Σ₀,ξ,n)
    
     crit =1.0; el0=0.0;numitr=0
-    open(homedir()*"/GIT/susie-glmm/SuSiEGLMM.jl/test/est_elbo0.txt","w") 
+    # open(homedir()*"/GIT/susie-glmm/SuSiEGLMM.jl/test/est_elbo0.txt","w") 
     #  open("./test/decELBO.txt","w")
     while (crit>=tol)
         ###check again!
@@ -292,15 +297,16 @@ struct Approx
 end
 
 ## EM for QTL
-function emAlt(yt,Xt,Xt₀,S,τ2,σ0::Float64,ξ,Σ₀,n::Int64;tol::Float64=1e-4)
+function emAlt(yt,Xt,Xt₀,S,τ2,σ0::Float64,ξ,Σ₀,n::Int64,c::Int64;tol::Float64=1e-4)
     
     
     # n,c=size(Xt₀)
     ghat =zeros(n); Vg = zeros(n);# λ = zeros(n)##
     
     ghat2=zeros(n); τ2_new=zeros(1); τ2 =[τ2]; 
-    b0=zeros(1); b1=zeros(1);b2=zeros(1); σ0_new =zeros(1); σ0=[σ0]
-    ξ_new = zeros(n); 
+    b0=zeros(1); b1=zeros(1);b2=zeros(1); σ1=zeros(1)
+    σ0_new =zeros(1); σ0=[σ0]
+    ξ_new = zeros(n); βhat=zeros(c)
     
     Xy₀=getXy('T',Xt₀,yt) #X₀'y
     # Vβ̂inv,Badj = covarAdj(Xy₀,yt,Xt₀,Σ₀,ξ,n)
@@ -317,7 +323,7 @@ function emAlt(yt,Xt,Xt₀,S,τ2,σ0::Float64,ξ,Σ₀,n::Int64;tol::Float64=1e-
          emB!(b2,σ0_new,b1,σ1)
          mStep!(ξ_new,b1,σ1,Vg,ghat,Badj,Xt₀,Xt,n)
 
-         el1=ELBO(ξ_new,τ2_new,σ0_new,Badj,b2,σ1,ghat,ghat2,Vg,S,Xt,Xy₀,Σ₀,n)
+         el1=ELBO(ξ_new,τ2_new,σ0_new,Badj,b1,b2,σ1,ghat,ghat2,Vg,S,Xt,Xy₀,Σ₀,n)
      
         #  if(el0>el1)
         #   f=open("./test/decELBO.txt","a")
@@ -327,12 +333,12 @@ function emAlt(yt,Xt,Xt₀,S,τ2,σ0::Float64,ξ,Σ₀,n::Int64;tol::Float64=1e-
          crit=abs(el1-el0)
         #  crit=norm(ξ_new-ξ)+norm(τ2_new-τ2)+abs(el1-el0)  
         
-         ξ=ξ_new; τ2=τ2_new;el0=el1;σ0=σ0_new
+         ξ=ξ_new; τ2=τ2_new;el0=el1;σ0=σ0_new;βhat=Badj.β̂
         
           numitr +=1        
     end
     println(numitr)
-    return Approx(Badj.β,b1,σ1,ξ,ghat,τ2[1],σ0_new[1],el0)
+    return Approx(βhat,b1,σ1,ξ,ghat,τ2[1],σ0_new[1],el0)
     
 end
 
@@ -344,12 +350,12 @@ end
 
     fitting H1 model
 """
-function glmm1Snp(n,τ0,σ_0,Σ0,yt::Vector{Float64},Xt::Vector{Float64},Xt₀::Union{Matrix{Float64},Vector{Float64}},S::Vector{Float64};tol=1e-4)
+function glmm1Snp(n,c,τ0,σ_0,Σ0,yt::Vector{Float64},Xt::Vector{Float64},Xt₀::Matrix{Float64},S::Vector{Float64};tol=1e-4)
 
     #initialization for ξ 
     ξ0=guessξ0(yt,Xt₀,Xt,S,Σ0,τ0,σ_0,n)
     
-    est0= emAlt(yt,Xt,Xt₀,S,τ0,σ_0,ξ0,Σ0,n;tol=tol)
+    est0= emAlt(yt,Xt,Xt₀,S,τ0,σ_0,ξ0,Σ0,n,c;tol=tol)
    
       return est0
 end
@@ -365,23 +371,22 @@ end
 function scan1SNP(y::Vector{Float64},X::Matrix{Float64},X₀::Union{Matrix{Float64},Vector{Float64}},T::Matrix{Float64},
     S::Vector{Float64};tol=1e-4)
  
-    n, nmar =size(X)
+    nmar =axes(X,2);n,c=size(X₀)
   # check if covariates are added as input and include the intercept. 
      if(X₀!= ones(n,1)) #&&(size(X₀,2)>1)
          X₀ = hcat(ones(n),X₀)
      end
      # initial values of hyper-prameters
      τ0 = 1.99; σ_0 = 1.0
-
+    #  est=zeros(nmar,2)
             Xt, Xt₀, yt = rotate(y,X,X₀,T)   
             Σ0= 2(cov(Xt₀)+I) # initial variance of prior β
-         est= @distributed (vcat) for j= nmar
-               est0= glmm1Snp(n,τ0,σ_0,Σ0,yt,Xt[:,j],Xt₀,S;tol=tol)
-                 [est0.ν est0]
-                 end
+         est=@distributed (vcat) for j in nmar
+               est0=glmm1Snp(n,c,τ0,σ_0,Σ0,yt,Xt[:,j],Xt₀,S;tol=tol)
+               [est0.ν est0]
+                                 end
  
-           return est[:,1] est[:,2]
-   
+           return est[:,1], est[:,2]
 
 end
 
